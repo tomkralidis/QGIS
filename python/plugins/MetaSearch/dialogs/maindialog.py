e194a2fa6497ed91156ce7b20e446e18222ed573
paul van genuchten <paul.vangenuchten@geocat.net>
2021-02-25 02:19:23 +0100

    adds service binding to oarec (#2)
    * adds service binding to oarec
    fixes boundingbox
    improvements metadata template
    
    * fix boundingbox regression

--- python/plugins/MetaSearch/dialogs/maindialog.py
+++ python/plugins/MetaSearch/dialogs/maindialog.py
@@ -556,6 +556,7 @@ class MetaSearchDialog(QDialog, BASE_CLASS):
             rt = QgsRectangle(float(bx['minx']), float(bx['miny']),
                               float(bx['maxx']), float(bx['maxy']))
             geom = QgsGeometry.fromRect(rt)
+            
             if geom is not None:
                 src = QgsCoordinateReferenceSystem("EPSG:4326")
                 dst = self.map.mapSettings().destinationCrs()
@@ -579,7 +580,7 @@ class MetaSearchDialog(QDialog, BASE_CLASS):
 
         services = {}
         for link in record['links']:
-
+            link = self.catalog.parse_link(link)
             if 'scheme' in link:
                 link_type = link['scheme']
             elif 'protocol' in link:
@@ -589,7 +590,7 @@ class MetaSearchDialog(QDialog, BASE_CLASS):
 
             if link_type is not None:
                 link_type = link_type.upper()
-
+            
             wmswmst_link_types = list(
                 map(str.upper, link_types.WMSWMST_LINK_TYPES))
             wfs_link_types = list(map(str.upper, link_types.WFS_LINK_TYPES))
